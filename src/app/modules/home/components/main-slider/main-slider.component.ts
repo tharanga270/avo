69c09eb6ab196859afeeb3b17e21a4f127a3cabe
tharanga270 <developerwordpress270@gmail.com>
2023-10-28 20:33:35 +0530

    2.main slider minor issue

--- src/app/modules/home/components/main-slider/main-slider.component.ts
+++ src/app/modules/home/components/main-slider/main-slider.component.ts
@@ -16,5 +16,16 @@ export class MainSliderComponent {
     mouseDrag: true,
     touchDrag: true,
     pullDrag: true,
+    responsive:{
+      0:{
+        items:1
+      },
+      600:{
+        items:1
+      },
+      1000:{
+        items:1
+      }
+    }
   };
 }
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -6,7 +6,10 @@ import { HomeComponent } from './home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MainSliderComponent } from './components/main-slider/main-slider.component';
 import { CarouselModule } from 'ngx-owl-carousel-o';
-import { BrowserAnimationsModule,NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+} from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
