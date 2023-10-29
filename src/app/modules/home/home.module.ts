5bd08fff135d4023191809c5538dd245144f5d7a
tharanga270 <developerwordpress270@gmail.com>
2023-10-29 19:51:58 +0530

    8.about-us row complete

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-//import { HttpClientModule } from '@angular/common/http';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MainSliderComponent } from './components/main-slider/main-slider.component';
 import { CarouselModule } from 'ngx-owl-carousel-o';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 @NgModule({
-  declarations: [HomeComponent, HeaderComponent, MainSliderComponent],
+  declarations: [
+    HomeComponent,
+    HeaderComponent,
+    MainSliderComponent,
+    AboutUsComponent,
+  ],
   imports: [CommonModule, HomeRoutingModule, CarouselModule],
 })
 export class HomeModule {}
