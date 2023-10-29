b80bfd7569e1e5b1b1c96596cd4b739cb082ef96
tharanga270 <developerwordpress270@gmail.com>
2023-10-29 13:05:13 +0530

    3.slider complete without responsive

--- src/app/modules/home/components/main-slider/main-slider.component.ts
+++ src/app/modules/home/components/main-slider/main-slider.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-main-slider',
   templateUrl: './main-slider.component.html',
   styleUrls: ['./main-slider.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MainSliderComponent {
   customOptions: any = {
