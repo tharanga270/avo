c9188032989c3ed669d2a7018562399f7791a5d8
tharanga270 <developerwordpress270@gmail.com>
2023-10-29 16:47:47 +0530

    5.main slider allmost

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -6,20 +6,9 @@ import { HomeComponent } from './home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MainSliderComponent } from './components/main-slider/main-slider.component';
 import { CarouselModule } from 'ngx-owl-carousel-o';
-import {
-  BrowserAnimationsModule,
-  NoopAnimationsModule,
-} from '@angular/platform-browser/animations';
-import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [HomeComponent, HeaderComponent, MainSliderComponent],
-  imports: [
-    CommonModule,
-    HomeRoutingModule,
-    CarouselModule,
-    //BrowserModule,
-    //BrowserAnimationsModule,
-  ],
+  imports: [CommonModule, HomeRoutingModule, CarouselModule],
 })
 export class HomeModule {}
