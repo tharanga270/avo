b6020bc3e1f0bdb8113d53ffa73ea94677a01d39
tharanga270 <developerwordpress270@gmail.com>
2023-11-01 01:00:38 +0530

    24.recent-blog section complete without responsive

--- src/app/modules/home/components/recent-blog/recent-blog.component.ts
+++ src/app/modules/home/components/recent-blog/recent-blog.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-recent-blog',
   templateUrl: './recent-blog.component.html',
-  styleUrls: ['./recent-blog.component.scss']
+  styleUrls: ['./recent-blog.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class RecentBlogComponent {
 
